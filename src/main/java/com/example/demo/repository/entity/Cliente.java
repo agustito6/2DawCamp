804b98b2c20197395f87294711e2be6c5d8b43a2
MigueLucky <miguejorda8@gmail.com>
2024-01-15 13:07:25 +0100

    Cambiado Cliente.java by YO

--- src/main/java/com/example/demo/repository/entity/Cliente.java
+++ src/main/java/com/example/demo/repository/entity/Cliente.java
@@ -15,7 +15,7 @@ public class Cliente {
     @Id  
     @GeneratedValue(strategy=GenerationType.IDENTITY)   
     private Long id;     
-    //MigueLucky manda v2
+    //MigueLucky manda v3
     @Column(name = "nif")   
     private String nif;    
     
